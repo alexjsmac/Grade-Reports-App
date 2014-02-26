22b4094726ca00cdce205f78cd1fac657991a3f3
Stephen Solis <stephsolis@gmail.com>
2014-02-26 12:31:30 -0500

    Add Maven file structure

--- src/main/java/ca/uwo/csd/cs2212/team10/Course.java
+++ src/main/java/ca/uwo/csd/cs2212/team10/Course.java
@@ -1,33 +1,33 @@
-package gradebook;
+package ca.uwo.csd.cs2212.team10;
 
 /**
  * Represents a course in the Gradebook
- * @author alexmaclean
+ * @author Team 10
  */
 public class Course {
-    
+
     /* Attributes */
     private String code;
     private String term;
     private String title;
-    
+
     /* Constructor */
     public void Course(String title, String code, String term){
         this.title = title;
         this.code = code;
         this.term = term;
     }
-    
+
     /* Public Methods */
-    
+
     public void setTitle(String newTitle){
         title = newTitle;
     }
-    
+
     public void setCode(String newCode){
         code = newCode;
     }
-    
+
     public void setTerm(String newTerm){
         term = newTerm;
     }
--- src/main/java/ca/uwo/csd/cs2212/team10/Gradebook.java
+++ src/main/java/ca/uwo/csd/cs2212/team10/Gradebook.java
@@ -1,31 +1,32 @@
-package gradebook;
-import java.util.*;
+package ca.uwo.csd.cs2212.team10;
+
+import java.util.ArrayList;
 
 /**
  * Represents the main Gradebook of the project
- * @author alexmaclean
+ * @author Team 10
  */
 public class Gradebook {
 
     /* Attributes */
     Course activeCourse;
-    ArrayList<Course> courses;      // Sequence of all courses Gradebook manages
-    
+    ArrayList<Course> courses; //The courses in the Gradebook
+
     /* Constructor */
     public void Gradebook(){
-    courses = new ArrayList();
-}
-    
+        courses = new ArrayList();
+    }
+
     /* Public Methods */
-    
+
     public void addCourse(Course course){
         courses.add(course);
     }
-    
+
     public void removeCourse(Course course){
         courses.remove(course);
     }
-    
+
     public void setActiveCourse(Course course){
         activeCourse = course;
     }
