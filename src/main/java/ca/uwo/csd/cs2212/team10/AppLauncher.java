6c2bbf487f138fccb275e76c0a96f633e377968e
Stephen Solis <stephsolis@gmail.com>
2014-02-27 12:53:49 -0500

    Added methods for persistence and changed app launch mechanism

---
+++ src/main/java/ca/uwo/csd/cs2212/team10/AppLauncher.java
@@ -0,0 +1,11 @@
+package ca.uwo.csd.cs2212.team10;
+
+/**
+ * The entry point of the gradebook program
+ * @author Team 10
+ */
+public class AppLauncher{
+	public static void main(String[] args){
+		new MainWindow(); //delegate to the constructor of MainWindow
+	}
+}
