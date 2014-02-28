bcfc191be462785bb12cfdac23098defeec094d5
Stephen Solis <stephsolis@gmail.com>
2014-02-28 14:51:45 -0500

    Added class descriptions

---
+++ class-descriptions/Deliverable.java
@@ -0,0 +1,84 @@
+package ca.uwo.csd.cs2212.team10;
+
+/**
+ * Class representing a gradeable item to be completed by a student in a course 
+ * 
+ * @author Team 10
+ */
+public class Deliverable {
+	
+	/*****************************************************************
+	 * Instance Variables
+	 ****************************************************************/
+	
+	// The name of the deliverable
+	private String name;
+	
+	// The type of deliverable
+	private String type;
+	
+	// The weight of the deliverable
+	private int weight;
+	
+	/**
+	 * Create a new deliverable with specified name, type, and weight
+	 * @param name The name of the deliverable
+	 * @param type The type of deliverable
+	 * @param weight The weight of the deliverable
+	 */
+	public Deliverable (String name, String type, int weight)
+	{
+	}
+	
+	/*****************************************************************
+	 * Public Methods
+	 ****************************************************************/
+	
+	/**
+	 * Sets name of deliverable
+	 * @param name The name to set
+	 */
+	public void setName(String name)
+	{
+	}
+	
+	/**
+	 * Sets type of deliverable
+	 * @param type The type to set
+	 */
+	public void setType(String type)
+	{
+	}
+	
+	/**
+	 * Sets weight of deliverable
+	 * @param weight The weight to set
+	 */
+	public void setWeight(int weight)
+	{
+	}
+	
+	/**
+	 * Gets name of deliverable
+	 * @return The name of the deliverable
+	 */
+	public String getName()
+	{
+	}
+	
+	/**
+	 * Gets type of deliverable
+	 * @return The type of deliverable
+	 */
+	public String getType()
+	{
+	}
+	
+	/**
+	 * Gets weight of deliverable
+	 * @return The weight of the deliverable
+	 */
+	public int getWeight()
+	{
+	}
+}
