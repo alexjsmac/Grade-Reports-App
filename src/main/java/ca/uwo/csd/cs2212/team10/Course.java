8b1ad44eee05c27c3c92b24cc9d8e6ab4d59e173
Henrique Brighenti <hbrighen@uwo.ca>
2014-03-12 07:54:04 -0400

    Implemented Spreadsheet

--- src/main/java/ca/uwo/csd/cs2212/team10/Course.java
+++ src/main/java/ca/uwo/csd/cs2212/team10/Course.java
@@ -6,10 +6,12 @@ import java.io.*;
 
 /**
  * Represents a course in the Gradebook
+ *
  * @author Team 10
  */
-public class Course implements Serializable{
+public class Course implements Serializable {
     /* Constants */
+
     private static final long serialVersionUID = 1L; //for serializing
 
     /* Attributes */
@@ -17,68 +19,71 @@ public class Course implements Serializable{
     private String term;
     private String title;
     private ArrayList<Student> students;
-	private ArrayList<Deliverable> deliverables;
+    private ArrayList<Deliverable> deliverables;
 
     /* Constructor */
-    public Course(String title, String code, String term){
+    public Course(String title, String code, String term) {
         this.title = title;
         this.code = code;
         this.term = term;
-		
+
         students = new ArrayList<Student>();
-		deliverables = new ArrayList<Deliverable>();
+        deliverables = new ArrayList<Deliverable>();
     }
 
     /* Public Methods */
-
-    public void setTitle(String title){
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setCode(String code){
+    public void setCode(String code) {
         this.code = code;
     }
 
-    public void setTerm(String term){
+    public void setTerm(String term) {
         this.term = term;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public String getCode(){
+    public String getCode() {
         return code;
     }
 
-    public String getTerm(){
+    public String getTerm() {
         return term;
     }
-    
-    public ArrayList<Student> getStudentList(){
-    	return students;
+
+    public ArrayList<Student> getStudentList() {
+        return students;
     }
-	
-	public ArrayList<Deliverable> getDeliverableList(){
-    	return deliverables;
+
+    public ArrayList<Deliverable> getDeliverableList() {
+        return deliverables;
     }
-    
-    public void addStudent(Student student){
-    	students.add(student);
+
+    public void addStudent(Student student) {
+        students.add(student);
     }
-    
-    public void removeStudent(Student student){
-    	students.remove(student);
+
+    public void removeStudent(Student student) {
+        students.remove(student);
     }
-	
-	public void addDeliverable(Deliverable deliverable){
-    	deliverables.add(deliverable);
+
+    public void addDeliverable(Deliverable deliverable) {
+        deliverables.add(deliverable);
+        //Creates one Grade object linked to the new Deliverable in every student of the Course
+        for (Student student : students) {
+            student.addGrade(new Grade(deliverable, 0));
+        }
     }
-    
-	public void removeDeliverable(Deliverable deliverable){
-    	deliverables.remove(deliverable);
+
+    public void removeDeliverable(Deliverable deliverable) {
+        deliverables.remove(deliverable);
     }
-    
+
     @Override
     public String toString() {
         return code + " - " + title;
---
+++ src/main/java/ca/uwo/csd/cs2212/team10/TableModel.java
@@ -0,0 +1,127 @@
+package ca.uwo.csd.cs2212.team10;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * Class to set the Model of the Student's Table
+ * @author team10
+ */
+
+public class TableModel extends AbstractTableModel {
+       
+    private final static int IDX_FIRST_NAME = 0;
+    private final static int IDX_LAST_NAME = 1;
+    private final static int IDX_STUDENT_NUMBER = 2;
+    private final static int IDX_EMAIL = 3;
+    private final static int IDX_AVG = 4;
+    private final static int IDX_ASSIG_AVG = 5;
+    private final static int IDX_EXAM_AVG = 6;
+    
+    private int COLUMN_COUNT;
+    private final ArrayList<Student> students;
+    private final ArrayList<Deliverable> deliverables;
+
+    public TableModel(ArrayList<Student> studentsList, ArrayList<Deliverable> deliverablesList) {
+        students = studentsList;
+        deliverables = deliverablesList;
+        COLUMN_COUNT = 7 + (deliverables.size());
+    }
+    
+    public List<Student> getStudentsList() {
+        return students;
+    }
+    
+    @Override
+    public int getRowCount() {
+        return students.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return COLUMN_COUNT;
+    }
+    
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        if (columnIndex == IDX_STUDENT_NUMBER)
+            return Integer.class;
+        else if (columnIndex > 3)
+            return Float.class;
+        else 
+            return String.class;
+    }
+    
+    @Override
+    public String getColumnName(int columnIndex) {
+        if (columnIndex == IDX_FIRST_NAME)
+            return "First Name";
+        else if (columnIndex == IDX_LAST_NAME)
+            return "Last Name";
+        else if (columnIndex == IDX_STUDENT_NUMBER)
+            return "Student Number";
+        else if (columnIndex == IDX_EMAIL)
+            return "Email";
+        else if (columnIndex == IDX_AVG)
+            return "Total Average";
+        else if (columnIndex == IDX_ASSIG_AVG)
+            return "Assignments Average";
+        else if (columnIndex == IDX_EXAM_AVG)
+            return "Exams Average";
+        else if ((columnIndex >= 7) && columnIndex < COLUMN_COUNT)
+            return deliverables.get((columnIndex - 7)).getName();
+        else 
+            return null;      
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        if ((rowIndex < 0) || (rowIndex >= students.size()))
+            return null;
+        
+        Student selectedStudent = students.get(rowIndex);
+        if (columnIndex == IDX_FIRST_NAME)
+            return selectedStudent.getFirstName();
+        else if (columnIndex == IDX_LAST_NAME)
+            return selectedStudent.getLastName();
+        else if (columnIndex == IDX_STUDENT_NUMBER)
+            return selectedStudent.getNum();
+        else if (columnIndex == IDX_EMAIL)
+            return selectedStudent.getEmail();
+        else if (columnIndex == IDX_AVG)
+            return selectedStudent.calcAverage();
+        else if (columnIndex == IDX_ASSIG_AVG)
+            return selectedStudent.calcAssignmentAverage();
+        else if (columnIndex == IDX_EXAM_AVG)
+            return selectedStudent.calcExamAverage();
+        else if ((columnIndex >= 5) && columnIndex < COLUMN_COUNT) {
+            Deliverable deliverable = deliverables.get((columnIndex - 7));
+            return selectedStudent.getGrade(deliverable).getGrade();
+        }
+        else 
+            return null;
+    }
+    
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        if ((rowIndex < 0) || (rowIndex >= students.size()) || columnIndex <= IDX_EXAM_AVG || columnIndex >= COLUMN_COUNT)
+            return;
+            
+        Student selectedStudent = students.get(rowIndex);
+        Deliverable deliverable = deliverables.get((columnIndex - 7));
+        selectedStudent.getGrade(deliverable).setGrade((Float)aValue);
+
+        fireTableCellUpdated(rowIndex, columnIndex);
+        fireTableDataChanged();
+    }
+    
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+      if (columnIndex > IDX_EXAM_AVG && columnIndex < COLUMN_COUNT)
+          return true;
+      else
+          return false;
+    }
+ 
+}
