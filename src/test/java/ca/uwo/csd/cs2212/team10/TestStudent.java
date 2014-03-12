ebdca8d3a19e1c1a9a721996cec801a270e21ba1
William Callaghan <wcallag3@uwo.ca>
2014-03-12 00:08:50 -0400

    teststudent class

---
+++ src/test/java/ca/uwo/csd/cs2212/team10/TestStudent.java
@@ -0,0 +1,48 @@
+package ca.uwo.csd.cs2212.team10;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.Before;
+
+public class TestStudent{
+	private Student student
+	
+	@Before
+	public void setup(){
+		student = new Student("John", "Doe", "jdoe@example.com",123456789);
+	}
+	
+	@Test
+	public void testConstructorSetsAttributes(){
+		student = new Student("John", "Doe", "jdoe@example.com", 123456789);
+		
+		Assert.assertEquals("John", student.getFirstName());
+		Assert.assertEquals("Doe", student.getLastName());
+		Assert.assertEquals("jdoe@example.com", student.getEmail());
+		Assert.assertEquals(123456789, student.getStudentNumber());
+	}
+	
+	@Test
+	public void testSetFirstName(){
+		student.setFirstName("Jane");
+		Assert.assertEquals("Jane", student.getFirstName());
+	}
+	
+	@Test
+	public void testSetLastName(){
+		student.setLastName("Foo");
+		Assert.assertEquals("Foo", student.getLastName());
+	}
+	
+	@Test
+	public void testSetEmail(){
+		student.setEmail("jfoo@example.com");
+		Assert.assertEquals("jfoo@example.com", student.getEmail());
+	}
+	
+	@Test
+	public void testSetStudentNumber(){
+		student.setStudentNumber(987654321);
+		Assert.assertEquals(987654321, student.getStudentNumber());
+	}
+}
