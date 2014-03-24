fbc00be3f7aa903765d17fbc44ad50bc30e478cb
Stephen Solis <stephsolis@gmail.com>
2014-03-24 17:25:51 -0400

    Made dialog boxes automatically resize

--- src/main/java/ca/uwo/csd/cs2212/team10/UserEntryPrompter.java
+++ src/main/java/ca/uwo/csd/cs2212/team10/UserEntryPrompter.java
@@ -33,30 +33,29 @@ public class UserEntryPrompter{
         showCourseDialog(parent, EDIT_TYPE, oldCourse.getTitle(), oldCourse.getCode(), oldCourse.getTerm());
     }
     
-    public void showAddDeliverableDialog(Component parent, Course course){
-        showDeliverableDialog(parent, ADD_TYPE, null, 0, null, course);
+    public void showAddDeliverableDialog(Component parent, Course containingCourse){
+        showDeliverableDialog(parent, ADD_TYPE, null, 0, null, containingCourse);
     }
     
-    public void showEditDeliverableDialog(Component parent, Deliverable oldDeliverable, Course course){
-        showDeliverableDialog(parent, EDIT_TYPE, oldDeliverable.getName(), oldDeliverable.getType(), ""+oldDeliverable.getWeight(), course);
+    public void showEditDeliverableDialog(Component parent, Deliverable oldDeliverable, Course containingCourse){
+        showDeliverableDialog(parent, EDIT_TYPE, oldDeliverable.getName(), oldDeliverable.getType(), ""+oldDeliverable.getWeight(), containingCourse);
     }
     
-    public void showAddStudentDialog(Component parent, Course course){
-        showStudentDialog(parent, ADD_TYPE, null, null, null, null, course, null);
+    public void showAddStudentDialog(Component parent, Course containingCourse){
+        showStudentDialog(parent, ADD_TYPE, null, null, null, null, containingCourse, null);
     }
     
-    public void showEditStudentDialog(Component parent, Student oldStudent, Course course){
-        showStudentDialog(parent, EDIT_TYPE, oldStudent.getFirstName(), oldStudent.getLastName(), oldStudent.getNum(), oldStudent.getEmail(), course, oldStudent);
+    public void showEditStudentDialog(Component parent, Student oldStudent, Course containingCourse){
+        showStudentDialog(parent, EDIT_TYPE, oldStudent.getFirstName(), oldStudent.getLastName(), oldStudent.getNum(), oldStudent.getEmail(), containingCourse, oldStudent);
     }
     
     private void showCourseDialog(Component parent, int dialogType, String oldTitle, String oldCode, 
                                     String oldTerm){
-        final int errorMsgLength = 55; //the amount of space to reserve for the error message
         
         final JTextField title = new JTextField(oldTitle);
         final JTextField code = new JTextField(oldCode);
         final JTextField term = new JTextField(oldTerm);
-        final JLabel errorMsg = new JLabel(new String(new char[errorMsgLength]).replace("\0", " "));
+        final JLabel errorMsg = new JLabel();
         
         final JButton ok = new JButton("OK");
         final JButton cancel = new JButton("Cancel");
@@ -67,11 +66,14 @@ public class UserEntryPrompter{
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (title.getText().trim().isEmpty()) {
-                    errorMsg.setText("You must enter a title.");
+                    errorMsg.setText("<html>You must enter a title.</html>");
+                    getParentDialog((Component)e.getSource()).pack();
                 } else if (code.getText().trim().isEmpty()) {
-                    errorMsg.setText("You must enter a course code.");
+                    errorMsg.setText("<html>You must enter a course code.</html>");
+                    getParentDialog((Component)e.getSource()).pack();
                 } else if (term.getText().trim().isEmpty()) {
-                    errorMsg.setText("You must enter a term.");
+                    errorMsg.setText("<html>You must enter a term.</html>");
+                    getParentDialog((Component)e.getSource()).pack();
                 } else{
                     getParentOptionPane((Component)e.getSource()).setValue(ok);
                 }
@@ -115,13 +117,12 @@ public class UserEntryPrompter{
     }
     
     private void showDeliverableDialog(Component parent, int dialogType, String oldName, int oldType, 
-                                        String oldWeight, final Course course){
-        final int errorMsgLength = 100; //the amount of space to reserve for the error message
+                                        String oldWeight, final Course containingCourse){
         
         final JTextField name = new JTextField(oldName);
         final JComboBox type = new JComboBox(Deliverable.TYPES);
         final JTextField weight = new JTextField(oldWeight);
-        final JLabel errorMsg = new JLabel("<html>" + new String(new char[errorMsgLength]).replace("\0", " ") + "<br> </html>");
+        final JLabel errorMsg = new JLabel();
         
         final JButton ok = new JButton("OK");
         final JButton cancel = new JButton("Cancel");
@@ -137,7 +138,8 @@ public class UserEntryPrompter{
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (name.getText().trim().isEmpty()) {
-                    errorMsg.setText("You must enter a name.");
+                    errorMsg.setText("<html>You must enter a name.</html>");
+                    getParentDialog((Component)e.getSource()).pack();
                 } else {
                     try {
                         //check that the weight is a positive integer
@@ -147,16 +149,18 @@ public class UserEntryPrompter{
                         
                         //check that the weights add up to less than 100
                         int total = 0;
-                        for (Deliverable d : course.getDeliverableList())
+                        for (Deliverable d : containingCourse.getDeliverableList())
                             total += d.getWeight();
                         if (total + newWeightVal - oldWeightVal > 100)
                             throw new IllegalArgumentException();
                         
                     } catch (NumberFormatException ex) {
-                        errorMsg.setText("You must enter a positive integer for the weight.");
+                        errorMsg.setText("<html>You must enter a positive integer for the weight.</html>");
+                        getParentDialog((Component)e.getSource()).pack();
                         return;
                     } catch (IllegalArgumentException ex){
                         errorMsg.setText("<html>The total weights in the course cannot add up to more than 100%.</html>");
+                        getParentDialog((Component)e.getSource()).pack();
                         return;
                     }
                     
@@ -202,14 +206,14 @@ public class UserEntryPrompter{
     }
 
     private void showStudentDialog(Component parent, int dialogType, String oldFirstName, String oldLastName, 
-                                    String oldNum, String oldEmail, final Course course, final Student student){
-        final int errorMsgLength = 100; //the amount of space to reserve for the error message
+                                    String oldNum, String oldEmail, final Course containingCourse, 
+                                    final Student student){
         
         final JTextField firstName = new JTextField(oldFirstName);
         final JTextField lastName = new JTextField(oldLastName);
         final JTextField number = new JTextField(oldNum);
         final JTextField email = new JTextField(oldEmail);
-        final JLabel errorMsg = new JLabel(new String(new char[errorMsgLength]).replace("\0", " "));
+        final JLabel errorMsg = new JLabel();
         
         final JButton ok = new JButton("OK");
         final JButton cancel = new JButton("Cancel");
@@ -220,21 +224,28 @@ public class UserEntryPrompter{
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (firstName.getText().trim().isEmpty()) {
-                    errorMsg.setText("You must enter a first name.");
+                    errorMsg.setText("<html>You must enter a first name.</html>");
+                    getParentDialog((Component)e.getSource()).pack();
                 } else if (lastName.getText().trim().isEmpty()) {
-                    errorMsg.setText("You must enter a last name.");
+                    errorMsg.setText("<html>You must enter a last name.</html>");
+                    getParentDialog((Component)e.getSource()).pack();
                 } else if (number.getText().trim().isEmpty()) {
-                    errorMsg.setText("You must enter a student number.");
+                    errorMsg.setText("<html>You must enter a student number.</html>");
+                    getParentDialog((Component)e.getSource()).pack();
                 } else if (!isValidEmail(email.getText())) {
-                    errorMsg.setText("You must enter a valid email address.");
+                    errorMsg.setText("<html>You must enter a valid email address.</html>");
+                    getParentDialog((Component)e.getSource()).pack();
                 } else {
                     try {
-                        course.validateStudentModification(student, email.getText(), number.getText());
+                        containingCourse.validateStudentModification(student, email.getText(), number.getText());
                     } catch (DuplicateStudentException ex) {
-                        if (ex.getReason() == DuplicateStudentException.DUP_NUMBER)
-                            errorMsg.setText("The student number entered is already used by another student.");
-                        else if (ex.getReason() == DuplicateStudentException.DUP_EMAIL)
-                            errorMsg.setText("The email address entered is already used by another student.");
+                        if (ex.getReason() == DuplicateStudentException.DUP_NUMBER){
+                            errorMsg.setText("<html>The student number entered is already used by another student.</html>");
+                            getParentDialog((Component)e.getSource()).pack();
+                        } else if (ex.getReason() == DuplicateStudentException.DUP_EMAIL){
+                            errorMsg.setText("<html>The email address entered is already used by another student.</html>");
+                            getParentDialog((Component)e.getSource()).pack();
+                        }
                         return;
                     }
                     
@@ -284,6 +295,16 @@ public class UserEntryPrompter{
         while (c != null) {
             if (c instanceof JOptionPane) {
                 return (JOptionPane) c;
+            }
+            c = c.getParent();
+        }
+        return null;
+    }
+    
+    private static JDialog getParentDialog(Component c) {
+        while (c != null) {
+            if (c instanceof JDialog) {
+                return (JDialog) c;
             }
             c = c.getParent();
         }
